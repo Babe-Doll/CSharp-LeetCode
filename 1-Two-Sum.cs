a5a14c8f95761f64bff5a692876a9f3cb08ce149
Babe-Doll <37282501+Babe-Doll@users.noreply.github.com>
2018-09-05 09:29:18 +0800

    1-Two-Sum

---
+++ 1-Two-Sum.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    public class Program
+    {
+        public int[] TwoSum(int[] nums, int target)
+        {
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = i+1; j < nums.Length; j++)
+                {
+                    if (nums[i] + nums[j] == target)
+                    {
+                        
+                        return new int[] { i, j };
+
+                    }
+
+                }
+            }
+            throw new ArgumentException();
+        }
+        static void Main(string[] args)
+        {
+            int[] nums = { 2, 7, 11, 15 };
+            Program pro = new Program();
+
+            int[] result = pro.TwoSum(nums, 9);
+            Console.WriteLine("[{0},{1}]",result[0],result[1]);
+            Console.ReadKey();
+        }
+    }
+}
